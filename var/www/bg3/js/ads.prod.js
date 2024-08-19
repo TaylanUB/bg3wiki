52fd4f52e43ccc0d49503757aca4deed23c83b46
Taylan Kammer <taylan.kammer@gmail.com>
2024-08-19 21:55:40 +0200

    update

--- var/www/bg3/js/ads.prod.js
+++ var/www/bg3/js/ads.prod.js
@@ -1,41 +1,55 @@
 (function(){
-  var classes = document.body.classList;
-  var height = window.innerHeight;
-  if (classes.contains('skin-citizen') && height < 720) {
-    console.log('Ads disabled because height < 720px.');
-    return;
-  }
+	var classes = document.body.classList;
+	var height = window.innerHeight;
+	if (classes.contains('skin-citizen') && height < 720) {
+		console.log('Ads disabled because height < 720px.');
+		return;
+	}
 
-  var query = new URLSearchParams(window.location.search);
-  var provider = query.get('ad_provider');
-  if (provider == null) {
-    if (Math.random() < 0.05) {
-      provider = 'playwire';
-    } else {
-      provider = 'publift';
-    }
-  }
+	var query = new URLSearchParams(window.location.search);
+	var provider = query.get('ad_provider');
+	if (provider == null) {
+		if (Math.random() < 0.05) {
+			provider = 'playwire';
+		} else {
+			provider = 'publift';
+		}
+	}
 
-  console.log("Enabling ads for: " + provider);
+	console.log("Enabling ads for: " + provider);
 
-  var notice = document.getElementById('bg3wiki-ad-provider-notice');
-  notice.innerText = 'Ads provided by: ' + provider;
+	var notice = document.getElementById('bg3wiki-ad-provider-notice');
+	notice.innerText = 'Ads provided by: ' + provider;
 
-  // For Playwire
-  window.ramp = {};
-  window.ramp.que = [];
+	if (provider == 'publift') {
+		// This is quite hacky but leave it like this until A/B test is done.
+		// The divs for Playwire lay on top of the Publift ones and prevent
+		// click-through events, so just remove them if provider is publift.
+		var header = document.getElementById('bg3wiki-header-ad-ramp');
+		var sidebar = document.getElementById('bg3wiki-sidebar-ad-ramp');
+		var footer = document.getElementById('bg3wiki-footer-ad-ramp');
+		for (const node of [ header, sidebar, footer ]) {
+			if (node != null) {
+				node.parentNode.removeChild(node);
+			}
+		}
+	}
 
-  var script = document.createElement('script');
-  if (provider == 'playwire') {
-    script.src = '//cdn.intergient.com/1025372/75208/ramp.js';
-  } else {
-    script.src = '//cdn.fuseplatform.net/publift/tags/2/3741/fuse.js';
-  }
-  script.onload = function(){
-    console.log('Done loading ads JS.');
-  };
-  script.onerror = function(){
-    console.log('Error loading ads JS.');
-  };
-  document.body.appendChild(script);
+	// For Playwire
+	window.ramp = {};
+	window.ramp.que = [];
+
+	var script = document.createElement('script');
+	if (provider == 'playwire') {
+		script.src = '//cdn.intergient.com/1025372/75208/ramp.js';
+	} else {
+		script.src = '//cdn.fuseplatform.net/publift/tags/2/3741/fuse.js';
+	}
+	script.onload = function(){
+		console.log('Done loading ads JS.');
+	};
+	script.onerror = function(){
+		console.log('Error loading ads JS.');
+	};
+	document.body.appendChild(script);
 })()
--- var/www/bg3/w/LocalSettings.prod.php
+++ var/www/bg3/w/LocalSettings.prod.php
@@ -356,55 +356,17 @@ $wgHooks['SkinAfterContent'][] = function( &$html, $skin ) {
 
 };
 
-$wgHooks['SkinAfterBottomScripts'][] = function ( $skin, &$html ) {
+$wgHooks['SkinAfterBottomScripts'][] = function ( $skin, &$html )
+	use ( $devSite )
+{
 	if ( !bg3wikiAdsEnabled( $skin->getOutput() ) ) {
 		return;
 	}
-	$html .= <<< EOF
-	<script>
-	(function(){
-	  var classes = document.body.classList;
-	  var height = window.innerHeight;
-	  if (classes.contains('skin-citizen') && height < 720) {
-	    console.log('Ads disabled because height < 720px.');
-	    return;
-	  }
-
-	  var query = new URLSearchParams(window.location.search);
-	  var provider = query.get('ad_provider');
-	  if (provider == null) {
-	    if (Math.random() < 0.1) {
-	      provider = 'playwire';
-	    } else {
-	      provider = 'publift';
-	    }
-	  }
-
-	  console.log("Enabling ads for: " + provider);
-
-	  var notice = document.getElementById('bg3wiki-ad-provider-notice');
-	  notice.innerText = 'Ads provided by: ' + provider;
-
-	  // For Playwire
-	  window.ramp = {};
-	  window.ramp.que = [];
-
-	  var script = document.createElement('script');
-	  if (provider == 'playwire') {
-	    script.src = '//cdn.intergient.com/1025372/75208/ramp.js';
-	  } else {
-	    script.src = '//cdn.fuseplatform.net/publift/tags/2/3741/fuse.js';
-	  }
-	  script.onload = function(){
-	    console.log('Done loading ads JS.');
-	  };
-	  script.onerror = function(){
-	    console.log('Error loading ads JS.');
-	  };
-	  document.body.appendChild(script);
-	})()
-	</script>
-	EOF;
+	if ( $devSite ) {
+		$html .= '<script src="/js/ads.dev.js"></script>';
+	} else {
+		$html .= '<script src="/js/ads.prod.js"></script>';
+	}
 };
 
 #
