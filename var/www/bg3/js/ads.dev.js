f58c76ce78c2d332cebabb8eef8f51bdf2fcd180
Taylan Kammer <taylan.kammer@gmail.com>
2024-08-19 22:08:11 +0200

    update

--- var/www/bg3/js/ads.dev.js
+++ var/www/bg3/js/ads.dev.js
@@ -21,20 +21,6 @@
 	var notice = document.getElementById('bg3wiki-ad-provider-notice');
 	notice.innerText = 'Ads provided by: ' + provider;
 
-	if (provider == 'publift') {
-		// This is quite hacky but leave it like this until A/B test is done.
-		// The divs for Playwire lay on top of the Publift ones and prevent
-		// click-through events, so just remove them if provider is publift.
-		var header = document.getElementById('bg3wiki-header-ad-ramp');
-		var sidebar = document.getElementById('bg3wiki-sidebar-ad-ramp');
-		var footer = document.getElementById('bg3wiki-footer-ad-ramp');
-		for (const node of [ header, sidebar, footer ]) {
-			if (node != null) {
-				node.parentNode.removeChild(node);
-			}
-		}
-	}
-
 	// For Playwire
 	window.ramp = {};
 	window.ramp.que = [];
