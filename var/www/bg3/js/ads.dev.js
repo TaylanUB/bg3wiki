9f66c1ad4765155993df37a96dd6f8d31c28dd82
Taylan Kammer <taylan.kammer@gmail.com>
2024-08-31 10:06:43 +0200

    update

--- var/www/bg3/js/ads.dev.js
+++ var/www/bg3/js/ads.dev.js
@@ -1,22 +1,23 @@
 (function(){
 
-let adsEnabled = false;
+if (innerWidth >= 320 && innerHeight >= 720) {
+	enableAds();
+} else {
+	addEventListener('resize', maybeEnableAds);
+}
 
-function maybeEnableAds(){
-	if (adsEnabled) {
-		return;
-	}
-
+function maybeEnableAds() {
 	if (innerWidth < 320 || innerHeight < 720) {
 		return;
 	}
 
 	enableAds();
-	adsEnabled = true;
+
+	removeEventListener('resize', maybeEnableAds);
 }
 
 function enableAds() {
-	const query = new URLSearchParams(window.location.search);
+	const query = new URLSearchParams(location.search);
 	let provider = query.get('ad_provider');
 	if (provider == null) {
 		if (Math.random() < 0.5) {
@@ -28,17 +29,30 @@ function enableAds() {
 
 	console.log("Enabling ads for: " + provider);
 
-	// For Playwire
-	window.ramp = {};
-	window.ramp.que = [];
+	loadAdScript(provider);
+
+	const notice = document.getElementById('bg3wiki-ad-provider-notice');
+	notice.innerText = 'Ads provided by: ' + provider;
+}
+
+function loadAdScript(provider) {
+	let src;
+	if (provider == 'playwire') {
+		ramp = {};
+		ramp.que = [];
+		ramp.passiveMode = true;
+		ramp.que.push(rampSetup);
+		src = '//cdn.intergient.com/1025372/75208/ramp.js';
+	} else {
+		fusetag = {};
+		fusetag.que = [];
+		fusetag.que.push(fuseSetup);
+		src = '//cdn.fuseplatform.net/publift/tags/2/3741/fuse.js';
+	}
 
 	const script = document.createElement('script');
 	script.async = true;
-	if (provider == 'playwire') {
-		script.src = '//cdn.intergient.com/1025372/75208/ramp.js';
-	} else {
-		script.src = '//cdn.fuseplatform.net/publift/tags/2/3741/fuse.js';
-	}
+	script.src = src;
 	script.onload = function(){
 		console.log('Done loading ads JS.');
 	};
@@ -46,13 +60,181 @@ function enableAds() {
 		console.log('Error loading ads JS.');
 	};
 	document.body.appendChild(script);
-
-	const notice = document.getElementById('bg3wiki-ad-provider-notice');
-	notice.innerText = 'Ads provided by: ' + provider;
 }
 
-maybeEnableAds();
+function rampSetup() {
+	const classes = document.body.classList;
+	if (!classes.contains('skin-citizen')) {
+		console.log('Not on mobile.');
+		ramp.spaNewPage();
+		return;
+	}
 
-addEventListener('resize', maybeEnableAds);
+	let newPage = true;
+
+	function setFooterXL() {
+		setFooter('footer-970-wide', 'standard_iab_foot2');
+	}
+
+	function setFooterL() {
+		setFooter('large-portrait-tablet', 'standard_iab_foot2');
+	}
+
+	function setFooterM() {
+		setFooter('ROS', 'standard_iab_foot1');
+	}
+
+	function setFooterS() {
+		setFooter('ROS', 'standard_iab_foot1');
+	}
+
+	function setNoFooter() {
+		setFooter('ROS', null);
+	}
+
+	function setFooter(path, type) {
+		console.log(`New Footer: ${path}/${type}`);
+
+		if (ramp.settings.cp == path) {
+			setType();
+		} else {
+			ramp.setPath(path).then(setType);
+		}
+
+		function setType() {
+			ramp.destroyUnits('all').then(() => {
+				if (type == null) {
+					if (newPage) {
+						ramp.spaNewPage();
+						newPage = false;
+					}
+					return;
+				}
+				ramp.addUnits({
+					type: type,
+					selectorId: 'bg3wiki-footer-ad-ramp',
+				}).then(() => {
+					if (newPage) {
+						ramp.spaNewPage();
+						newPage = false;
+					} else {
+						ramp.displayUnits();
+					}
+				});
+			});
+		}
+	}
+
+	const sizes = [
+		[ [970, 1024], setFooterXL ],
+		[ [728, 1024], setFooterL  ],
+		[ [468,  720], setFooterM  ],
+		[ [320,  720], setFooterS  ],
+	];
+
+	function footerSetterForScreenSize() {
+		for (const entry of sizes) {
+			const dimens = entry[0];
+			const setter = entry[1];
+			const w = dimens[0];
+			const h = dimens[1];
+			if (innerWidth >= w && innerHeight >= h) {
+				return setter;
+			}
+		}
+		return setNoFooter;
+	}
+
+	let setter = footerSetterForScreenSize();
+	setter();
+
+	function onResize() {
+		const newSetter = footerSetterForScreenSize();
+		if (setter != newSetter) {
+			setter = newSetter;
+			setter();
+		}
+	}
+
+	let timeout;
+	const delay = 200;
+	function debouncedOnResize() {
+		clearTimeout(timeout);
+		timeout = setTimeout(onResize, delay);
+	}
+
+	addEventListener('resize', debouncedOnResize);
+}
+
+function fuseSetup() {
+	const classes = document.body.classList;
+	if (!classes.contains('skin-citizen')) {
+		console.log('Not on mobile.');
+		return;
+	}
+
+	// IDs to be changed
+	const footerFuseIds = [
+		[ [970, 1024], '23198268151' ],
+		[ [728, 1024], '23198268151' ],
+		[ [468,  720], '23198268151' ],
+		[ [320,  720], '23198268151' ],
+	];
+
+	function footerFuseIdForScreenSize() {
+		for (const entry of footerFuseIds) {
+			const dimens = entry[0];
+			const fuseId = entry[1];
+			const w = dimens[0];
+			const h = dimens[1];
+			if (innerWidth >= w && innerHeight >= h) {
+				return fuseId;
+			}
+		}
+		return null;
+	}
+
+	function replaceFooterAdZone(fuseId) {
+		console.log('Replacing footer fuseId to: ' + fuseId);
+
+		const outerDivId = 'bg3wiki-footer-ad';
+		const innerDivId = 'bg3wiki-footer-ad-fuse';
+
+		const outerDiv = document.getElementById(outerDivId);
+		const innerDiv = document.getElementById(innerDivId);
+		if (innerDiv != null) {
+			outerDiv.removeChild(innerDiv);
+		}
+
+		if (fuseId != null) {
+			const newDiv = document.createElement('div');
+			newDiv.id = innerDivId;
+			newDiv.setAttribute('data-fuse', fuseId);
+			outerDiv.appendChild(newDiv);
+
+			fusetag.registerZone(innerDivId);
+		}
+	}
+
+	let footerFuseId = footerFuseIdForScreenSize();
+	replaceFooterAdZone(footerFuseId);
+
+	function onResize() {
+		const newFuseId = footerFuseIdForScreenSize();
+		if (footerFuseId != newFuseId) {
+			footerFuseId = newFuseId;
+			replaceFooterAdZone(footerFuseId);
+		}
+	}
+
+	let timeout;
+	const delay = 200;
+	function debouncedOnResize() {
+		clearTimeout(timeout);
+		timeout = setTimeout(onResize, delay);
+	}
+
+	addEventListener('resize', debouncedOnResize);
+}
 
 })()
--- var/www/bg3/js/ads.prod.js
+++ var/www/bg3/js/ads.prod.js
@@ -1,18 +1,19 @@
 (function(){
 
-let adsEnabled = false;
+if (window.innerWidth >= 320 && window.innerHeight >= 720) {
+	enableAds();
+} else {
+	addEventListener('resize', maybeEnableAds);
+}
 
-function maybeEnableAds(){
-	if (adsEnabled) {
-		return;
-	}
-
-	if (innerWidth < 320 || innerHeight < 720) {
+function maybeEnableAds() {
+	if (window.innerWidth < 320 || window.innerHeight < 720) {
 		return;
 	}
 
 	enableAds();
-	adsEnabled = true;
+
+	removeEventListener('resize', maybeEnableAds);
 }
 
 function enableAds() {
@@ -28,31 +29,133 @@ function enableAds() {
 
 	console.log("Enabling ads for: " + provider);
 
-	// For Playwire
-	window.ramp = {};
-	window.ramp.que = [];
+	if (provider == 'playwire') {
+		window.ramp = {};
+		window.ramp.que = [];
+		window.ramp.passiveMode = true;
+		window.ramp.que.push(rampSetup);
+	} else {
+		window.fusetag = {};
+		window.fusetag.que = [];
+//		window.fusetag.que.push(fuseSetup);
+	}
 
 	const script = document.createElement('script');
 	script.async = true;
+
 	if (provider == 'playwire') {
 		script.src = '//cdn.intergient.com/1025372/75208/ramp.js';
 	} else {
 		script.src = '//cdn.fuseplatform.net/publift/tags/2/3741/fuse.js';
 	}
+
 	script.onload = function(){
 		console.log('Done loading ads JS.');
 	};
+
 	script.onerror = function(){
 		console.log('Error loading ads JS.');
 	};
+
 	document.body.appendChild(script);
 
 	const notice = document.getElementById('bg3wiki-ad-provider-notice');
 	notice.innerText = 'Ads provided by: ' + provider;
 }
 
-maybeEnableAds();
+function rampSetup() {
+	const ramp = window.ramp;
 
-addEventListener('resize', maybeEnableAds);
+	if (ramp.settings.device !== 'tablet') {
+		ramp.spaNewPage();
+		return;
+	}
+
+	if (window.innerWidth >= 728 && window.innerHeight >= 1024) {
+		ramp.setPath('large-portrait-tablet').then(() => ramp.spaNewPage());
+	} else {
+		ramp.spaNewPage();
+	}
+
+	rampRegisterScreenResizeHandler();
+}
+
+function rampRegisterScreenResizeHandler() {
+	const ramp = window.ramp;
+
+	const thresholdWidth = 728;
+	const thresholdHeight = 1024;
+
+	let previousWidth = window.innerWidth;
+	let previousHeight = window.innerHeight;
+
+	let timeout;
+	const delay = 200;
+	function debouncedOnResize() {
+		clearTimeout(timeout);
+		timeout = setTimeout(onResize, delay);
+	}
+
+	window.addEventListener('resize', debouncedOnResize);
+
+	function onResize() {
+		const width = window.innerWidth;
+		const height = window.innerHeight;
+
+		if (!ramp.settings.slots) {
+			return;
+		}
+
+		const slots = Object.keys(ramp.settings.slots);
+		if (slots.some(key => key.includes("foot"))) {
+			checkFooterSizeChange(width, height);
+		}
+
+		previousWidth = width;
+		previousHeight = height;
+	}
+
+	function checkFooterSizeChange(width, height) {
+		if ((previousWidth < thresholdWidth && width >= thresholdWidth) ||
+			(previousHeight < thresholdHeight && height >= thresholdHeight)) {
+
+			console.log("Screen size >= 728x1024");
+			setFooterLargeSize();
+
+		} else if ((previousWidth >= thresholdWidth && width < thresholdWidth) ||
+				   (previousHeight >= thresholdHeight && height < thresholdHeight)) {
+
+			console.log("Screen size < 728x1024");
+			setFooterSmallSize();
+
+		}
+	}
+
+	function setFooterLargeSize() {
+		ramp.setPath('large-portrait-tablet').then(() => {
+			ramp.destroyUnits('all').then(() => {
+				ramp.addUnits({
+					type: 'standard_iab_foot2',
+					selectorId: 'bg3wiki-footer-ad-ramp',
+				}).then(() => {
+					ramp.displayUnits();
+				});
+			});
+		});
+	}
+
+	function setFooterSmallSize() {
+		ramp.setPath('ROS').then(() => {
+			ramp.destroyUnits('all').then(() => {
+				ramp.addUnits({
+					type: 'standard_iab_foot1',
+					selectorId: 'bg3wiki-footer-ad-ramp',
+				}).then(() => {
+					ramp.displayUnits();
+				});
+			});
+		});
+	}
+}
 
 })()
